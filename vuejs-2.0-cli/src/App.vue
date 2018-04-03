28ecaee015c3d78b45809c0a30ecf765e683fb33
ChenAllen <andycc77e@gmail.com>
2018-04-04 01:50:44 +0800

    使用 Vue 脚手架工具进行组件化开发

---
+++ vuejs-2.0-cli/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <todos :todos="todos"></todos>
+    <todo-form :todos="todos"></todo-form>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Todos from './components/Todos'
+import TodoForm from './components/TodoForm'
+export default {
+  name: 'App',
+  data(){
+    return {
+      message:'Hello World',
+      todos:[
+        {id:1, title:'Learn Vuejs', completed:false}
+      ]
+    }
+  },
+  computed:{
+    todoCount(){
+      return this.todos.length;
+    }
+  },
+  components: {
+    HelloWorld,
+    Todos,
+    TodoForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuejs-2.0-cli/src/components/Todos.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <ul class="list-group">
+        <li class="list-group-item"
+            v-for="(todo, index) in todos"
+            :class="{'completed':todo.completed}">
+          {{ todo.title }}
+          <button
+            class="btn btn-xs pull-right margin-right-10"
+            @click="toggleCompletion(todo)"
+            :class="[todo.completed? 'btn-danger':'btn-success']"
+          >{{ todo.completed? 'undo':'completed' }}</button>
+          <button
+            class="btn btn-warning btn-xs pull-right"
+            @click="deleteTodo(index)"
+          >Delete</button>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "todos",
+        props: ['todos'],
+        methods:{
+          deleteTodo(index){
+            this.todos.splice(index,1)
+          },
+          toggleCompletion(todo){
+            todo.completed = !todo.completed
+          }
+        }
+    }
+</script>
+
+<style scoped>
+  .completed{
+    color: #5cb85c;
+    text-decoration: line-through;
+  }
+
+  .margin-right-10{
+    margin-right: 10px;
+  }
+</style>
